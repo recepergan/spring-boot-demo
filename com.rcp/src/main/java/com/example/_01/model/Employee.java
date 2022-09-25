981937bbc84fdff85d1c2719624a235d0a8ea439
recepergan <68998200+recepergan@users.noreply.github.com>
2022-09-25 12:14:30 +0300

    JPA project for Spring Boot. Data loaded

---
+++ com.rcp/src/main/java/com/example/_01/model/Employee.java
@@ -0,0 +1,73 @@
+package com.example._01.model;
+
+import org.springframework.stereotype.Controller;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "fullname")
+    private String fullName;
+    private double salary;
+    @Column(name = "phonenumber")
+    private String phoneNumber;
+
+    public Employee() {
+    }
+
+    public Employee(String fullName, double salary, String phoneNumber) {
+        this.fullName = fullName;
+        this.salary = salary;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return phoneNumber == employee.phoneNumber;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "fullName='" + fullName + '\'' +
+                ", salary=" + salary +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
