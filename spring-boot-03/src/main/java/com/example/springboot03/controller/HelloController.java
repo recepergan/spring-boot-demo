f422d06f608c563a8535a1f9cf54610662ada24f
recepergan <68998200+recepergan@users.noreply.github.com>
2022-10-02 17:26:47 +0300

    thymeleaf added..

---
+++ spring-boot-03/src/main/java/com/example/springboot03/controller/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.springboot03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "helloo";
+    }
+
+    @GetMapping("/hellodev/{name}")
+    public String sayHelloToDeveloper(@PathVariable("name") String name, Model model) {
+        model.addAttribute("developername",name);
+        model.addAttribute("servertime", LocalDateTime.now());
+        return "hellodev";
+    }
+}
